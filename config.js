a9d65a83aa3595ce8df4cd1ea43914be91421d04
Fatchurohman <fatur62@gmail.com>
2026-09-04 10:30:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,27 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+/**
+ * Konfigurasi Pusat Firebase - Pasar Candi Gebang Permai
+ * Developer: fatur62
+ */
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6-iEAKeoUtUfUxmsXD1sMHh296JyVo-g",
-  authDomain: "candi-gebang-lapak.firebaseapp.com",
-  projectId: "candi-gebang-lapak",
-  storageBucket: "candi-gebang-lapak.firebasestorage.app",
-  messagingSenderId: "81271558870",
-  appId: "1:81271558870:web:6bd654b68b00b7fbb76f77",
-  measurementId: "G-GV8R59H03N"
+    apiKey: "AIzaSyD6-iEAKeoUtUfUxmsXD1sMH...",
+    authDomain: "candi-gebang-lapak.firebaseapp.com",
+    projectId: "candi-gebang-lapak",
+    storageBucket: "candi-gebang-lapak.appspot.com",
+    messagingSenderId: "81271558870",
+    appId: "1:81271558870:web:6bd654b68b00b7fbb76f77",
+    measurementId: "G-GV8R59H03N"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inisialisasi Firebase secara aman (Compat Mode)
+let db = null;
+try {
+    if (window.firebase && firebaseConfig.projectId) {
+        firebase.initializeApp(firebaseConfig);
+        db = firebase.firestore();
+    } else {
+        console.error("Pustaka Firebase belum dimuat atau konfigurasi kosong.");
+    }
+} catch (error) {
+    console.error("Gagal menginisialisasi Firebase:", error);
+}
